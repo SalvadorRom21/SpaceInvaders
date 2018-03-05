6cdd36f1530d5ed3a8a17d46019a06ece8292818
TheHeartlessOne <romer.salvador.am@gmail.com>
2018-03-05 09:57:33 -0600

    SpaceInvaders first run

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,6 +1,13 @@
-#new name
-print ("hello world")
-#This is a new change 
-#this is another test yolo be the moto 
+#Sapce Invdavers 
+#Python 3.5.2 on linux
+#Setting up the Screen
 
-#this is also a new change #this is a  brnach test
+import turtle 
+import os
+import tkinter
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title ("Space Invaders")
+
+delay = raw_input ("Press enter to finish.")
