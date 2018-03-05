eaaae96ee8f876902faaaf1d389334264a8b44c2
TheHeartlessOne <romer.salvador.am@gmail.com>
2018-03-05 11:14:54 -0600

    Git branch merging test

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -2,12 +2,4 @@
 #Python 3.5.2 on linux
 #Setting up the Screen
 
-import turtle 
-import os
-import tkinter
-
-wn = turtle.Screen()
-wn.bgcolor("black")
-wn.title ("Space Invaders")
-
-delay = raw_input ("Press enter to finish.")
+print ("Testing branch workings")
